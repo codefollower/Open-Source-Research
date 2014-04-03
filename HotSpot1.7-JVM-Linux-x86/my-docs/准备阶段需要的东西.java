04a9c8f5eff8cb1c5633cf676941b4660622d2be
Honghua Zhu <zhh200910@gmail.com>
2014-04-03 13:58:55 +0800

    增加HotSpot1.7-JVM-Linux-x86历史项目

---
+++ HotSpot1.7-JVM-Linux-x86/my-docs/准备阶段需要的东西.java
@@ -0,0 +1,46 @@
+1. 软件:
+------------
+
+1.1 Ubuntu 12.04.4 LTS
+
+64位
+
+
+1.2 Eclipse IDE for C/C++ Developers
+
+https://www.eclipse.org/downloads/
+
+选择Linux 64 Bit
+
+
+
+1.3 OpenJDK
+
+https://jdk7.java.net/source.html
+http://www.java.net/download/openjdk/jdk7u40/promoted/b43/openjdk-7u40-fcs-src-b43-26_aug_2013.zip
+
+
+
+2. 学习资料:
+------------
+
+2.1 GNU make手册:
+
+中文: http://blogimg.chinaunix.net/blog/upfile/070907021605.pdf
+英文: http://www.gnu.org/software/make/manual/
+
+
+2.2 C/C++语言教材:
+
+<<C程序设计语言>>: http://book.douban.com/subject/1139336/
+<<C++ Primer Plus, 6th Edition>>: http://book.douban.com/subject/10789789/
+
+
+2.3 JVM相关:
+
+<<深入Java虚拟机(原书第2版)>>: http://book.douban.com/subject/1138768/
+
+<<Java Virtual Machine Specification>>
+
+中文: http://www.iteye.com/topic/1117824
+英文: http://docs.oracle.com/javase/specs/jvms/se7/jvms7.pdf
